eb3754fb577c21f1f7c41d20ab30c4130c240a78
Kaan HIRÇIN <74260613+kaanhircin@users.noreply.github.com>
2022-09-07 10:54:46 +0300

    JavaScript Info

---
+++ JavaScript Information/main.js
@@ -0,0 +1,18 @@
+// Alert message screen
+alert("Welcome to JavaScript Awareness Website!");
+
+var number1 = 10,
+    number2 = 20,
+    result = number1 + number2;
+
+// Alert message screen
+alert("Result: " + result);
+
+// Write on page
+document.write("Result: " + result);
+
+// JavaScript function
+function WriteSecondParagraph() {
+
+    document.getElementById("paragraph").innerHTML = "Hello JavaScript!";
+}
